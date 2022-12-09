a9ff7d679e50ecf757be7e156d897f1b6d4deea3
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-09 11:31:34 +0400

    Added task37

---
+++ task37/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Найдите произведение пар чисел в одномерном массиве.
+Парой считается первый и последний элемент, второй и предпоследний и т. д.
+Результат записать в новый массив.
+
+[1,2,3,4,5] -> 5,8,3
+[6,7,3,6] -> 36,21*/
+
+int[] createArray()
+{
+    int n = new Random().Next(2, 10);
+    int[] array = new int[n];
+    for(int i = 0; i < n; i++){
+        array[i] = new Random().Next(-100, 100);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+
+    return array;
+}
+
+int[] printPairMull(int[] arr)
+{
+    int[] newArr;
+    if((arr.Length % 2) == 0){
+        newArr = new int[arr.Length/2];
+        for(int i = 0; i < newArr.Length; i++){
+            newArr[i] = arr[i] * arr[(arr.Length - 1) - i];
+            Console.Write($"{newArr[i]} ");
+        }
+    }
+    else{
+        newArr = new int[arr.Length/2 + 1];
+        for(int i = 0; i < newArr.Length; i++){
+            if(i == (newArr.Length - 1)){
+                newArr[i] = arr[i];
+                Console.Write($"{newArr[i]} ");
+            }
+            else{ 
+                newArr[i] = arr[i] * arr[(arr.Length - 1) - i];
+                Console.Write($"{newArr[i]} ");
+            }
+        }
+    }
+
+    return newArr;
+}
+
+int[] a = createArray();
+printPairMull(a);
